13575fadf54b97da44442116f9240baeb5f4381f
Michelle <tranm5@uci.edu>
2018-06-21 11:14:44 -0700

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -17,7 +17,7 @@ function showSlides() {
     }
     slides[slideIndex - 1].style.display = "block";
     indicators[slideIndex - 1].className += " active-indicator";
-    setTimeout(showSlides, 5000); // Change image every 2 seconds
+    setTimeout(showSlides, 5000);
 }
 
 var slideIndex = 1;
